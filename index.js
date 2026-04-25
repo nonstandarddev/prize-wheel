377c44c271c7968ce0bc058411ab26aea9e9eed3
nonstandarddev <nonstandarddev@gmail.com>
2026-04-25 12:30:28 +0100

    Added logic to determine end-result

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 /*
     1: Constants & helpers
 */
-const OPTIONS = ["Option 1", "Option 2", "Option 3", "Option 4"];
 
-const initOptionMapping = () => {
-  const step = 360 / OPTIONS.length;
+// Constants
 
+const OPTIONS = [
+  "10% Off",
+  "It's Free!",
+  "Not Your Day",
+  "Bad Luck",
+  "No Luck",
+  "No Prize",
+  "Jog On",
+  "No Prize",
+];
+const OPTION_COLOURS = ["#ad0040ff", "#e77ba3"];
+const STEP_PCT = (1 / OPTIONS.length) * 100;
+const STEP_DEG = 360 / OPTIONS.length;
+const OPTION_MAPPING = () => {
   const mapping = new Map();
   OPTIONS.forEach((option, i) => {
-    mapping.set([i * step, (i + 1) * step], option);
+    mapping.set([i * STEP_DEG, (i + 1) * STEP_DEG], option);
   });
 
   return mapping;
@@ -19,10 +31,10 @@ const initOptionMapping = () => {
     
     NB: may be defined in one or more variables
 */
-let state = {
+const state = {
   rotation: {
-    position: "0deg",
-    mapping: initOptionMapping(),
+    position: 0,
+    ranges: OPTION_MAPPING(),
   },
 };
 
@@ -33,17 +45,32 @@ let state = {
 */
 
 // Setters
-let setRotationPosition = () => {
-  if (state.rotation.position === "0deg") {
-    const end = Math.floor(Math.random() * 360) + "deg";
+
+const setRotationPosition = () => {
+  if (state.rotation.position === 0) {
+    const end = Math.floor(Math.random() * 360);
     state.rotation.position = end;
   } else {
-    state.rotation.position = "0deg";
+    state.rotation.position = 0;
   }
 };
 
 // Getters
-let getRotationPosition = () => state.rotation.position;
+
+const getRotationPosition = () => state.rotation.position;
+const getRotationResult = (endState) => {
+  const ranges = state.rotation.ranges;
+  let selectedOption = null;
+  ranges.keys().forEach((range) => {
+    const lower = range[0];
+    const upper = range[1];
+    endStateAC = 360 - endState;
+    if (endStateAC >= lower && endStateAC < upper) {
+      selectedOption = ranges.get(range);
+    }
+  });
+  return selectedOption;
+};
 
 /*
     4: DOM References
@@ -52,7 +79,6 @@ let getRotationPosition = () => state.rotation.position;
 
 const DOM = {
   wheel: document.querySelector(".wheel"),
-  segments: document.querySelectorAll(".wheel li"),
   spin: document.querySelector(".spin"),
 };
 
@@ -60,20 +86,32 @@ const DOM = {
     5: DOM Updates (i.e. logic for 'modifying' the DOM in certain detailed ways)
 */
 
-const spinWheel = () => {
+const spinWheel = (endState) => {
   // Apply `.rotating` class to trigger CSS animation
   DOM.wheel.classList.toggle("rotating");
 
   // Set CSS variables `--random-end-state` to stop the wheel at a 'random' orientation
-  const endState = getRotationPosition();
-  DOM.wheel.style.setProperty("--random-end-state", endState);
+  DOM.wheel.style.setProperty("--random-end-state", endState + "deg");
 };
 
 const initView = () => {
   let accum = 0;
-  DOM.segments.forEach((segment) => {
+  OPTIONS.forEach((option, i) => {
+    const segment = document.createElement("li");
+    segment.setAttribute("data-percentage", STEP_PCT);
+    segment.setAttribute(
+      "data-color",
+      i % 2 === 0 ? OPTION_COLOURS[0] : OPTION_COLOURS[1],
+    );
     segment.style.setProperty("--accum", accum);
-    accum += parseFloat(segment.getAttribute("data-percentage"));
+
+    const alias = document.createElement("strong");
+    alias.textContent = option;
+
+    segment.appendChild(alias);
+    DOM.wheel.appendChild(segment);
+
+    accum += STEP_PCT;
   });
 };
 
@@ -86,12 +124,12 @@ const initView = () => {
 const onSpin = () => {
   // Update state
   setRotationPosition();
-
+  const endState = getRotationPosition();
   // Update DOM
-  spinWheel();
+  spinWheel(endState);
+  const endResult = getRotationResult(endState);
+  console.log(endResult);
 };
-
-const onLoad = () => {};
 
 /*
     7: Event Bindings
